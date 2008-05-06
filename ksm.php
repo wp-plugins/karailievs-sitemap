65cb386f3af8c5b82777b3cbb3148f016365f53c
karailiev <karailiev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-06 20:56:25 +0000

    v 0.4
    git-svn-id: https://plugins.svn.wordpress.org/karailievs-sitemap/tags/0.4@44606 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ksm.php
@@ -0,0 +1,282 @@
+<?php
+	/*
+		Plugin Name: Karailiev's sitemap
+		Plugin URI: http://www.karailiev.net/karailievs-sitemap/
+		Description: Generates sitemap for users and for spiders.
+		Version: 0.4
+		Author: Valentin Karailiev
+		Author URI: http://www.karailiev.net/
+	*/
+	$ksm_sitemap_version = "0.4";
+	$ksm_path = ABSPATH;
+	$ksm_sitemap_name = "sitemap.xml";
+	$ksm_sitemap_path = $ksm_path . $ksm_sitemap_name;
+
+	// Add some default options if they don't exist
+	add_option('ksm_active', true);
+	add_option('ksm_comments', false);
+	add_option('ksm_attachments', true);
+	add_option('ksm_categories', true);
+	add_option('ksm_tags', true);
+
+	// Get options for form fields
+	$ksm_active = stripslashes(get_option('ksm_active'));
+	$ksm_comments = stripslashes(get_option('ksm_comments'));
+	$ksm_attachments = stripslashes(get_option('ksm_attachments'));
+	$ksm_categories = stripslashes(get_option('ksm_categories'));
+	$ksm_tags = stripslashes(get_option('ksm_tags'));
+
+	// Add hooks
+	add_action('admin_menu', 'ksm_add_pages');
+	if ($ksm_active) {
+		add_action('edit_post', 'ksm_generate_sitemap');
+		add_action('delete_post', 'ksm_generate_sitemap');
+		add_action('private_to_published', 'ksm_generate_sitemap');
+		add_action('publish_page', 'ksm_generate_sitemap');
+		add_action('publish_phone', 'ksm_generate_sitemap');
+		add_action('publish_post', 'ksm_generate_sitemap');
+		add_action('save_post', 'ksm_generate_sitemap');
+		add_action('xmlrpc_publish_post', 'ksm_generate_sitemap');
+
+		if ($ksm_comments) {
+			add_action('comment_post', 'ksm_generate_sitemap');
+			add_action('edit_comment', 'ksm_generate_sitemap');
+			add_action('delete_comment', 'ksm_generate_sitemap');
+			add_action('pingback_post', 'ksm_generate_sitemap');
+			add_action('trackback_post', 'ksm_generate_sitemap');
+			add_action('wp_set_comment_status', 'ksm_generate_sitemap');
+		}
+
+		if ($ksm_attachments) {
+			add_action('add_attachment', 'ksm_generate_sitemap');
+			add_action('edit_attachment', 'ksm_generate_sitemap');
+			add_action('delete_attachment', 'ksm_generate_sitemap');
+		}
+	}
+
+	//Add config page
+	function ksm_add_pages() {
+		add_options_page("Karailiev's sitemap", "Sitemap", 8, basename(__FILE__), "ksm_admin_page");
+	}
+
+
+	function ksm_permissions() {
+		global $ksm_sitemap_path, $ksm_path;
+
+		if (is_file($ksm_sitemap_path)){
+			if (is_writable($ksm_sitemap_path)) $ksm_permission = 1;
+			else $ksm_permission = 3;
+		}
+		elseif (is_writable($ksm_path)) {
+			$ksm_permission = 2;
+			$fp = fopen($ksm_sitemap_path, 'w');
+			fwrite($fp, "<?xml version=\"1.0\" encoding=\"UTF-8\"?><urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" />");
+			fclose($fp);
+			if (is_file($ksm_sitemap_path)) $ksm_permission = 1;
+		}
+		else $ksm_permission = 4;
+
+		return $ksm_permission;
+	}
+
+
+	function ksm_generate_sitemap() {
+		global $ksm_sitemap_path, $ksm_sitemap_version, $table_prefix;
+
+		$ksm_permission = ksm_permissions();
+		if ($ksm_permission > 1) return;
+
+		mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+		mysql_query("SET NAMES '".DB_CHARSET."'");
+		mysql_select_db(DB_NAME);
+
+		$t = $table_prefix;
+		$ksm_active = stripslashes(get_option('ksm_active'));
+		$ksm_comments = stripslashes(get_option('ksm_comments'));
+		$ksm_categories = stripslashes(get_option('ksm_categories'));
+		$ksm_tags = stripslashes(get_option('ksm_tags'));
+
+		$urls = array();
+
+		$home = stripslashes(get_option('home')) . "/";
+		$result = mysql_query("
+			SELECT `post_modified`
+			FROM `".$t."posts`
+			WHERE
+				(`post_type`='page' OR `post_type`='post')
+				AND `post_status`='publish'
+			ORDER BY `post_modified` DESC
+			LIMIT 1
+		");
+		$data = mysql_fetch_assoc($result);
+		$homeLastUpdate = $data['post_modified'];
+		$homeLastUpdate = substr($homeLastUpdate, 0, 10);
+
+		$result = mysql_query("
+			SELECT `".$t."posts`.`ID`, `".$t."posts`.`post_modified`, `".$t."posts`.`post_name`
+			FROM `".$t."posts`
+			WHERE
+				(`".$t."posts`.`post_type`='page' OR `".$t."posts`.`post_type`='post')
+				AND `".$t."posts`.`post_status`='publish'
+			ORDER BY `".$t."posts`.`post_modified` DESC
+		");
+
+
+		while ($data = mysql_fetch_assoc($result)) {
+			$date = substr($data['post_modified'], 0, 10);
+
+			if ($ksm_comments) {
+				$cresult = mysql_query("
+					SELECT `".$t."comments`.`comment_date`
+					FROM `".$t."comments`
+					WHERE
+						`".$t."comments`.`comment_post_ID`='".$data['ID']."'
+						AND `".$t."comments`.`comment_approved`='1'
+					ORDER BY `".$t."comments`.`comment_date` DESC
+					LIMIT 1
+				");
+
+				if (mysql_num_rows($cresult) > 0) {
+					$cdata = mysql_fetch_assoc($cresult);
+					$commentDate = getdate(strtotime($cdata['comment_date']));
+					$postDate = getdate(strtotime($data['post_modified']));
+					if ($commentDate[0] > $postDate[0]) $date = substr($cdata['comment_date'], 0, 10);
+				}
+			}
+
+			$urls[$data['ID']] = array(
+				"url"		=> get_permalink($data['ID']),
+				"lastmod"	=> $date,
+				"changes"	=> "weekly",
+				"priority"	=> "0.3"
+			);
+		}
+
+		if ($ksm_categories || $ksm_tags) {
+			$what_kind = "";
+			if ($ksm_categories) $what_kind = "`".$t."term_taxonomy`.`taxonomy`='category'";
+			if ($ksm_tags) {
+				if ($what_kind == "") $what_kind = "`".$t."term_taxonomy`.`taxonomy`='post_tag'";
+				else $what_kind = "(" . $what_kind . " OR `".$t."term_taxonomy`.`taxonomy`='post_tag')";
+			}
+
+			$result = mysql_query("
+				SELECT `".$t."term_taxonomy`.`term_id`, `".$t."term_taxonomy`.`taxonomy`
+				FROM `".$t."term_taxonomy`
+				WHERE
+					`".$t."term_taxonomy`.`count` > 0
+					AND ".$what_kind."
+			");
+			while ($data = mysql_fetch_assoc($result)) {
+				if ($data['taxonomy'] == "category") $u = get_category_link($data['term_id']);
+				elseif ($data['taxonomy'] == "post_tag") $u = get_tag_link($data['term_id']);
+				else continue;
+				$urls['testm_'.$data['term_id']] = array(
+					"url"		=> $u,
+					"changes"	=> "weekly",
+					"priority"	=> "0.1"
+				);
+			}
+		}
+
+		$out = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+		$out .= "\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">
+	<!-- Generated by Karailiev's sitemap ".$ksm_sitemap_version." plugin -->
+	<!-- http://www.karailiev.net/ -->";
+		if (!$ksm_active) $out .= "
+	<!-- Plugin is off. Showing only homepage. -->";
+
+		$out .= "
+	<url>
+		<loc>".$home."</loc>
+		<lastmod>".$homeLastUpdate."</lastmod>
+		<changefreq>weekly</changefreq>
+		<priority>1.0</priority>
+	</url>";
+
+		if ($ksm_active) {
+			foreach ($urls as $u) {
+				$out .= "
+	<url>
+		<loc>".$u['url']."</loc>
+		".(isset($u['lastmod'])?"<lastmod>".$u['lastmod']."</lastmod>":'')."
+		<changefreq>".$u['changes']."</changefreq>
+		<priority>".$u['priority']."</priority>
+	</url>";
+			}
+		}
+
+		$out .= "\n</urlset>";
+		$fp = fopen($ksm_sitemap_path, 'w');
+		fwrite($fp, $out);
+		fclose($fp);
+	}
+
+
+
+	//Config page
+	function ksm_admin_page() {
+		$msg = "";
+
+		// Check form submission and update options
+		if ('ksm_submit' == $_POST['ksm_submit']) {
+			update_option('ksm_active', $_POST['ksm_active']);
+			update_option('ksm_comments', $_POST['ksm_comments']);
+			update_option('ksm_attachments', $_POST['ksm_attachments']);
+			update_option('ksm_categories', $_POST['ksm_categories']);
+			update_option('ksm_tags', $_POST['ksm_tags']);
+			ksm_generate_sitemap();
+		}
+		$ksm_active = stripslashes(get_option('ksm_active'));
+		$ksm_comments = stripslashes(get_option('ksm_comments'));
+		$ksm_attachments = stripslashes(get_option('ksm_attachments'));
+		$ksm_categories = stripslashes(get_option('ksm_categories'));
+		$ksm_tags = stripslashes(get_option('ksm_tags'));
+
+		$ksm_permission = ksm_permissions();
+		if ($ksm_permission == 3) $msg = "Error: sitemap.xml file exists but is not writable. <a href=\"http://www.karailiev.net/karailievs-sitemap/\" target=\"_blank\" >For help see the plugin's homepage</a>.";
+		elseif ($ksm_permission == 4) $msg = "Error: sitemap.xml file does not exist and the plugin can not create it. <a href=\"http://www.karailiev.net/karailievs-sitemap/\" target=\"_blank\" >For help see the plugin's homepage</a>.";
+		elseif ($ksm_permission == 2) $msg = "Error: sitemap.xml file does not exist and the plugin can not create it. <a href=\"http://www.karailiev.net/karailievs-sitemap/\" target=\"_blank\" >For help see the plugin's homepage</a>.";
+?>
+	<div class="wrap">
+<?php	if ($msg) {	?>
+	<div id="message" class="error"><p><strong><?php echo $msg; ?></strong></p></div>
+<?php	}	?>
+		<form name="form1" method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>&amp;updated=true">
+			<input type="hidden" name="ksm_submit" value="ksm_submit" />
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row">Sitemap settings</th>
+					<td>
+						<label for="ksm_active">
+							<input name="ksm_active" type="checkbox" id="ksm_active" value="1" <?php echo $ksm_active?'checked="checked"':''; ?> />
+							Acivate sitemap plugin.
+						</label><br />
+						<br />
+						<label for="ksm_categories">
+							<input name="ksm_categories" type="checkbox" id="ksm_categories" value="1" <?php echo $ksm_categories?'checked="checked"':''; ?> />
+							Show categories.
+						</label><br />
+						<label for="ksm_tags">
+							<input name="ksm_tags" type="checkbox" id="ksm_tags" value="1" <?php echo $ksm_tags?'checked="checked"':''; ?> />
+							Show tags.
+						</label><br />
+						<label for="ksm_comments">
+							<input name="ksm_comments" type="checkbox" id="ksm_comments" value="1" <?php echo $ksm_comments?'checked="checked"':''; ?> />
+							Use comments' dates.
+						</label><br />
+						<label for="ksm_attachments">
+							<input name="ksm_attachments" type="checkbox" id="ksm_attachments" value="1" <?php echo $ksm_attachments?'checked="checked"':''; ?> />
+							Rebuild on attachments modifications.
+						</label><br />
+					</td>
+				</tr>
+			</table>
+			<p class="submit">
+				<input type="submit" value="Save &amp; Rebuild" />
+			</p>
+		</form>
+	</div>
+<?php
+	}
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+﻿=== Karailiev's sitemap ===
+Contributors: Valentin Karailiev
+Tags: seo, sitemap, google, yahoo, msn, xml sitemap, xml
+Requires at least: 2.5
+Tested up to: 2.5.1
+Stable tag: trunk
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=valentin%40karailiev%2enet&item_name=karailievs%2dsitemap&no_shipping=1&no_note=1&tax=0&currency_code=EUR&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8
+
+IMPORTANT: versions 0.2.3+ fix some fatal bugs. Using older versions is NOT recommended!!!
+
+This plugin adds a XML sitemap to your blog.
+It's used to show all your pages and posts to the search engines like Google, Yahoo and MSN.
+
+== Description ==
+
+IMPORTANT: versions 0.2.3+ fix some fatal bugs. Using older versions is NOT recommended!!!
+
+This plugin adds a XML sitemap to your blog.
+It's used to show all your pages and posts to the search engines like Google, Yahoo and MSN.
+
+
+Changes in version 0.4:
+* Restore lost MySQL server connection (reported by [Matteo](http://www.italiasw.com/))
+* Categories added to the sitemap;
+* Tags added to the sitemap;
+
+
+Changes in version 0.3:
+* There is an option to turn on or off sitemap rebuilding when comments are changed (submit/edit/delete);
+* There is an option to turn on or off sitemap rebuilding when attachments are changed (upload/edit/delete);
+
+
+Changes in version 0.2:
+* Sitemap file has the usual name `sitemap.xml`;
+* Plugin checks if the file exists and if it's writable;
+* Sitemap is generated only when content changes (new/edit/delete post/page/comment/attachment);
+* Sitemap generates URLs according the permalinks settings
+
+== Installation ==
+
+1. Upload `karailievs-sitemap` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Create a file named sitemap.xml in your blog folder. The file must be writable for the web server. See more instructions on [plugin's homepage](http://www.karailiev.net/karailievs-sitemap/)
+1. Turn the sitemap on via the Settings -> Sitemap screen
+1. Open your sitemap to test it (e.g. http://www.karailiev.net/sitemap.xml)
